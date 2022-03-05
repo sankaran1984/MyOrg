5dcebea1fb4f9256a339fe25b3caa6555a94852c
Sankaran Nepolean <snepolean@skedulo.com>
2022-03-05 19:53:59 +0000

    added LWC challenge 1

---
+++ sodexo-app/main/default/lwc/sdxParentContractNewEdit/sdxParentContractNewEdit.js
@@ -0,0 +1,9 @@
+import { LightningElement, api } from 'lwc';
+
+export default class SdxParentContractNewEdit extends LightningElement {
+    @api recordId;
+
+    connectedCallback() {
+        
+    }
+}
